06a6926d057a8b9654309bd57816e65a145e05f2
Shivam Kumar <shivamk@policyadvisor.com>
2025-04-03 16:32:31 +0530

    Shivam - Added support for custom delimiters in add method

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -21,9 +21,19 @@ export default {
     add(numbers) {
       if (!numbers) return 0;
 
-      const numArray = numbers.split(/,|\n/).map((num) => parseInt(num, 10));
+      let delimiter = /,|\n/;
+      if (numbers.startsWith("//")) {
+        const delimiterMatch = numbers.match(/^\/\/(.+)\n/);
+        if (delimiterMatch) {
+          delimiter = new RegExp(delimiterMatch[1]);
+          numbers = numbers.split("\n").slice(1).join("\n");
+        }
+      }
+
+      const numArray = numbers.split(delimiter).map(num => parseInt(num, 10));
       return numArray.reduce((sum, num) => sum + num, 0);
-    },
+    }
+
   },
 };
 </script>
