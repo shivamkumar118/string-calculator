bbe927b95b9ae32cffae9598d606af812178cdbd
Shivam Kumar <shivamk@policyadvisor.com>
2025-04-03 17:01:18 +0530

    Shivam - Removed the boiler plate code and imported string calculator

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <StringCalculator />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StringCalculator from "./components/StringCalculator.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    StringCalculator
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
