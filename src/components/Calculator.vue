e866f20f994aa1dbef7815fe20f0deb971b46a52
Shivam Kumar <shivamk@policyadvisor.com>
2025-04-03 16:13:58 +0530

    Shivam - Added calculator component with default structure

---
+++ src/components/Calculator.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>String Calculator</h1>
+    <input placeholder="Enter numbers" />
+    <button>Calculate</button>
+    <p>Result:</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      result: null
+    };
+  },
+};
+</script>
